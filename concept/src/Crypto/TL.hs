22b5bfbcacafed2e4e03d5905e921c6e4f75ba2e
Charlie Durham <cpdurham@protonmail.com>
2021-03-31 20:35:03 -0400

    Some type safety for variable length sha256 return value

--- concept/src/Crypto/TL.hs
+++ concept/src/Crypto/TL.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Crypto.TL 
-  ( solveChain
-  , createChain
-  ) where
+  --( solveChain
+  --, createChain
+  --) where
+    where
+
+import Basement.Types.Word256 (Word256(..))
 
 import Crypto.Hash
 import Crypto.Cipher.AES (AES256)
@@ -19,25 +24,55 @@ import qualified Data.ByteString.Builder as Builder
 
 import Data.ByteString.Lazy (toStrict)
 
+import Data.Either (fromRight)
+
 import Data.Foldable (foldlM)
 
 import Data.List.NonEmpty (NonEmpty(..))
 
+import Data.Serialize
+
 import Data.Word (Word64)
 
-newtype Hash = Hash { unHash :: ByteString }
+instance Serialize Word256 where
+  put (Word256 w0 w1 w2 w3) =
+    do
+      putWord64be w0
+      putWord64be w1
+      putWord64be w2
+      putWord64be w3
+  get =
+    do
+      w0 <- getWord64be
+      w1 <- getWord64be
+      w2 <- getWord64be
+      w3 <- getWord64be
+      pure $ Word256 w0 w1 w2 w3
+
+newtype BS256 = BS256 { unBS256 :: ByteString }
   deriving Eq
 
-instance Show Hash where
-  show (Hash bs) = show $ Base16.encode bs
+instance Serialize BS256 where
+  put = put . bs256ToWord256
+    where
+      bs256ToWord256 :: BS256 -> Word256
+      bs256ToWord256 = fromRight undefined . decode . unBS256
+  get = fmap word256ToBS256 get
+    where
+      word256ToBS256 :: Word256 -> BS256
+      word256ToBS256 = BS256 . encode
+
+instance Show BS256 where
+  show = show . Base16.encode . unBS256
+
+newtype Hash = Hash { unHash :: BS256 }
+  deriving (Eq, Serialize, Show)
 
 newtype Checkpoint = Checkpoint Hash
-  deriving (Eq, Show)
+  deriving (Eq, Serialize, Show)
 
-newtype EncryptedHash = EncryptedHash ByteString
-
-instance Show EncryptedHash where
-  show (EncryptedHash bs) = show $ Base16.encode bs
+newtype EncryptedHash = EncryptedHash BS256
+  deriving (Serialize, Show)
 
 data Tower = 
   Tower
@@ -59,6 +94,14 @@ data Chain = Chain Int EncryptedHash Checkpoint Chain | Empty
 
 data CryptoTLError = MatchingIssue Checkpoint Hash | CryptoError CE.CryptoError
   deriving Show
+
+numLinks :: Chain -> Int
+numLinks = numLinks' 0
+  where
+    numLinks' :: Int -> Chain -> Int
+    numLinks' i Empty = i
+    numLinks' i (Chain _ _ _ c) = i' `seq` numLinks' i' c
+      where i' = i+1
 
 -- Module Export
 
@@ -104,35 +147,35 @@ randomHashTowers n i = sequence $ nonEmptyReplicate n (randomHashTower i)
 randomHashTower :: Int -> IO Tower
 randomHashTower i = 
   do
-    h <- Hash <$> CRT.getRandomBytes 32
+    h <- Hash . BS256 <$> CRT.getRandomBytes 32
     pure $ Tower { towerSize = i, towerStart = h, towerEnd = iterate' i sha256iter h}
 
 -- Crypto Stuff
 
 sha256 :: ByteString -> Hash
-sha256 = Hash . ByteArray.convert . hashWith SHA256
+sha256 = Hash . BS256 . ByteArray.convert . hashWith SHA256
 
 sha256iter :: Hash -> Hash
-sha256iter = sha256 . unHash
+sha256iter = sha256 . unBS256 . unHash
 
 -- Add 1 to each byte (with overflow) and then hash to yield our checkpoint
 sha256checkpoint :: Hash -> Checkpoint
-sha256checkpoint = Checkpoint . sha256 . BS.map (+1) . unHash
+sha256checkpoint = Checkpoint . sha256 . BS.map (+1) . unBS256 . unHash
 
 verifyCheckpoint :: Checkpoint -> Hash -> Bool
 verifyCheckpoint checkpoint hash = checkpoint == sha256checkpoint hash
 
 encryptHash :: Hash -> Hash -> Either CE.CryptoError EncryptedHash
 encryptHash hashKey hashMsg = 
-  fmap (\c -> EncryptedHash $ ecbEncrypt c $ unHash hashMsg) i
+  fmap (\c -> EncryptedHash $ BS256 $ ecbEncrypt c $ unBS256 $ unHash hashMsg) i
     where i :: Either CE.CryptoError AES256
-          i = CE.eitherCryptoError $ cipherInit $ unHash hashKey
+          i = CE.eitherCryptoError $ cipherInit $ unBS256 $ unHash hashKey
 
 decryptHash :: Hash -> EncryptedHash -> Either CE.CryptoError Hash
 decryptHash hashKey (EncryptedHash msg) = 
-  fmap (\c -> Hash $ ecbDecrypt c msg) i
+  fmap (\c -> Hash . BS256 . ecbDecrypt c $ unBS256 msg) i
     where i :: Either CE.CryptoError AES256
-          i = CE.eitherCryptoError $ cipherInit $ unHash hashKey
+          i = CE.eitherCryptoError $ cipherInit $ unBS256 $ unHash hashKey
 
 -- Helpers
 
